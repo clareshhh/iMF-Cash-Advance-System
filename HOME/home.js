dabb6ec1462e0f0a961187eaa1beecaca7e7c1b7
Ryle Tan <francesryletan0623@gmail.com>
2025-05-07 23:51:47 +0800

    updated search for transaction name only

--- HOME/home.js
+++ HOME/home.js
@@ -25,27 +25,55 @@ clearButton.addEventListener("click", () => {
 });
 
 
+// SEARCH BUT FOR EVERY TEXT IN THE CARD
+// const searchInput = document.getElementById('searchTextbox');
+// const clearBtn = document.getElementById('clearSearch');
 
-const searchInput = document.getElementById('searchTextbox');
-const clearBtn = document.getElementById('clearSearch');
+// function filterCards() {
+//   const searchedWord = searchInput.value.toLowerCase().trim();
+//   const cards = document.querySelectorAll('.card');
 
-function filterCards() {
-  const searchedWord = searchInput.value.toLowerCase().trim();
-  const cards = document.querySelectorAll('.card');
+//   cards.forEach(card => {
+//     const cardText = card.innerText.toLowerCase();
+//     if (cardText.includes(searchedWord)) {
+//       card.classList.remove('hide');
+//     } else {
+//       card.classList.add('hide');
+//     }
+//   });
+// }
+
+// searchInput.addEventListener('input', filterCards);
+
+// clearBtn.addEventListener('click', () => {
+//   searchInput.value = '';
+//   filterCards();
+// });
+
+// SEARCH BUT FOR THE TRANSACTION NAME ONLY
+const searchInput = document.getElementById("searchTextbox");
+const clearBtn = document.getElementById("clearSearch");
+
+function filterCards(){
+  const searchedWord = searchInput.value.toLowerCase().trim()
+  const cards = document.querySelectorAll(".card");
 
   cards.forEach(card => {
-    const cardText = card.innerText.toLowerCase();
-    if (cardText.includes(searchedWord)) {
-      card.classList.remove('hide');
-    } else {
-      card.classList.add('hide');
-    }
+  const eachTransactionName = card.querySelector(".transactionName"); //use card.querySelector cuz we are inside the forEach loop
+  const transactionName = eachTransactionName.innerText.toLowerCase().trim();
+
+      if(transactionName.includes(searchedWord)) {
+        card.classList.remove('hide');
+      } else {
+        card.classList.add('hide');
+      }
+
   });
 }
-
+  
 searchInput.addEventListener('input', filterCards);
 
-clearBtn.addEventListener('click', () => {
-  searchInput.value = '';
+clearBtn.addEventListener("click", () => {
+  searchInput.value = "";
   filterCards();
 });
